e54c3f373aaf302065532e2c5b7ef2d59ea15e69
Jesse Hernandez <futcomker@gmail.com>
2025-08-12 18:08:46 -0700

    docs: transfer to use ActiveMQMessaging only.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import ActiveMQMessaging from "./component/ActiveMQMessaging";
 
 
+import React from 'react';
+
 function App() {
   return (
     <div className="App">
+      <h1>Golf Sim Monitor</h1>
       <ActiveMQMessaging />
     </div>
   );
 }
+
+export default App;
